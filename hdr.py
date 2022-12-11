7147dc65429063247a01dad2e1e14ce9a32255e9
JKosciukiewicz <qubakosciukiewicz@gmail.com>
2022-12-11 15:07:16 +0100

    iuhiuh

--- hdr.py
+++ hdr.py
@@ -13,7 +13,6 @@ def create_hdr(img_samples,img_exposure_times_data):
     img_exposure_times=numpy.array(img_exposure_times_data, dtype=numpy.float32)
     img_list_nosize=[cv2.imread(img) for img in img_samples]
     img_list=[cv2.resize(img, (512,512)) for img in img_list_nosize]
-    img_list=[cv2.imread(img) for img in img_samples]
     #Create HDR images using different methods
     get_debeveck(img_list,img_exposure_times)
     get_robertson(img_list,img_exposure_times)
